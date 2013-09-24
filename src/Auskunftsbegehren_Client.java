d9fb463fe42b11ad3fe1c1f86e6d54f52175d5ac
Matthias Fassl <mf@n0g.at>
2013-09-24 19:48:31 +0200

    Read Addresses from STDIN

--- src/Auskunftsbegehren_Client.java
+++ src/Auskunftsbegehren_Client.java
@@ -1,19 +1,48 @@
 import java.lang.Exception;
+import java.util.Scanner;
 
 public class Auskunftsbegehren_Client
 {
+	public Address readAddress(Scanner sc, String identifier)
+	{
+		String firstName,lastName,street,number,postalcode,city,country;
+
+		System.out.println("Enter Details for " + identifier + " Address:\n");
+		System.out.println("Firstname: ");
+		firstName = sc.nextLine();
+		System.out.println("Lastname: ");
+		lastName = sc.nextLine();
+		System.out.println("Street: ");
+		street = sc.nextLine();
+		System.out.println("Street number: ");
+		number = sc.nextLine();
+		System.out.println("Postal Code: ");
+		postalcode = sc.nextLine();
+		System.out.println("City: ");
+		city = sc.nextLine();
+		System.out.println("Country: ");
+		country = sc.nextLine();
+		
+		return new Address(firstName,lastName,street,number,postalcode,city,country);
+	}
+	
 	public static void main(String[] args)
 	{
-		// TODO: read addresses from cli
-		Address from = new Address("Hermann","Wilhelm","Christophgasse","13/5","1050","Wien","Österreich");
-		Address to = new Address("Maximilia","Mustermann","Sternenstraße","7a","1234","Wien","Österreich");
+		//Address from = new Address("Hermann","Wilhelm","Christophgasse","13/5","1050","Wien","Österreich");
+		//Address to = new Address("Maximilia","Mustermann","Sternenstraße","7a","1234","Wien","Österreich");
 
+		Auskunftsbegehren_Client abc = new Auskunftsbegehren_Client();
+		Scanner scanner = new Scanner(System.in);
+		Address from = abc.readAddress(scanner,"Sender");
+		Address to = abc.readAddress(scanner,"Recipient");
+		System.out.println("Path for generated PDF: ");
+		String path = scanner.nextLine();
 
 		try {
 			Auskunftsbegehren ab = new Auskunftsbegehren();
 			ab.setSender(from);
 			ab.setRecipient(to);
-			ab.save("test.pdf");
+			ab.save(path);
 		}
 		catch(Exception e) {
 			e.printStackTrace();
