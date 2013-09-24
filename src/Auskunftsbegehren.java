94c6e1bf95b8be63c2891f13a49edc60cfeb972c
Matthias Fassl <mf@n0g.at>
2013-09-24 18:52:20 +0200

    Refactored Letter/Auskunftsbegehren

--- src/Auskunftsbegehren.java
+++ src/Auskunftsbegehren.java
@@ -2,15 +2,27 @@ import java.lang.Exception;
 
 public class Auskunftsbegehren
 {
+	private static final String SUBJECT = "Auskunftsbegehren gemäß DSG § 26";
+	private static final String CONTENT = "Gemäß DSG 2000 begehre ich Auskunft über alle über mich gespeicherten\npersonenbezogenen Daten.\n\nDie Auskunft hat die verarbeiteten Daten, die Informationen über ihre Herkunft, allfällige\nEmpfänger oder Empfängerkreise von Übermittlungen, den Zweck der Datenverwendung\nsowie die Rechtsgrundlagen hiefür in allgemein verständlicher Form anzuführen.\n\nGemäß DSG besteht eine Verpflichtung zur Auskunft binnen 8 Wochen.";
+	private static final String GREETING = "Mit freundlichen Grüßen";
+	private static final String NOTICE = "Hinweis: Dieses Dokument ist (mittels Bürgerkarte) mit einer qualifizierten elektronischen Signatur\nversehen, diese ist laut SigG § 4 rechtlich einer händischen Unterschrift gleichgestellt. Diese Signatur\nkann unter anderem unter http://www.signaturpruefung.gv.at überprüft werden. Eine qualifizierte\nelektronische Signatur ist, da ich als Signator unter alleiniger Kontrolle des Schlüssels bin, ein\nIdentitätsnachweis im Sinne von DSG §26 (1)";
+
+
 	public static void main(String args[])
 	{
 		try {
-			DINLetter dl = new DINLetter();
 			Address from = new Address("Hermann","Wilhelm","Christophgasse","13/5","1050","Wien","Österreich");
 			Address to = new Address("Maximilia","Mustermann","Sternenstraße","7a","1234","Wien","Österreich");
-			String subject = "Auskunftsbegehren gemäß DSG § 26";
-			dl.generateLetter(from,to,subject);
-			dl.saveTo("test.pdf");
+
+			DINLetter dl = new DINLetter();
+			dl.setSender(from);
+			dl.setRecipient(to);
+			dl.setSubject(SUBJECT);
+			dl.setContent(CONTENT);
+			dl.setGreeting(GREETING,from);
+			dl.setNotice(NOTICE);
+			dl.setFoldingmark(true);
+			dl.save("test.pdf");
 		}
 		catch(Exception e) {
 			e.printStackTrace();
--- src/DINLetter.java
+++ src/DINLetter.java
@@ -12,13 +12,13 @@ import java.util.Calendar;
 public class DINLetter
 {
 	private PDDocument document;
-	private PDFont font = PDType1Font.TIMES_ROMAN;
+	private PDFont fontNormal = PDType1Font.TIMES_ROMAN;
 	private PDFont fontBold = PDType1Font.TIMES_BOLD;
 	private PDFont fontItalic = PDType1Font.TIMES_ITALIC;
 	private int fontsize = 12;
 	private int smallFontsize = 10;
 	private int tinyFontsize = 8;
-	private float linespace = 1.1f;
+	private static final float LINESPACE = 1.1f;
 
 	private static final int DIN_CONTENT_LEFT = 71;
 	private static final int DIN_SENDER_ADDR_TOP = 751;
@@ -27,6 +27,7 @@ public class DINLetter
 
 	private static final int LETTER_SUBJECT_TOP = DIN_CONTENT_TOP-25;
 	private static final int LETTER_CONTENT_TOP = LETTER_SUBJECT_TOP-25;
+	private static final int LETTER_DATE_LEFT = 420;
 
 	private static final int DIN_FOLD1_TOP = 298;
 	private static final int DIN_FOLD2_TOP = 595;
@@ -37,151 +38,100 @@ public class DINLetter
 	private int noticeTop;
 
 	Address sender, recipient;
-	String Subject;
+	String subject;
+	String cityAndDate;
 	String[] content;
 	String[] notice;
 	String[] greeting;
+	boolean foldingmark;
+	boolean isNoticeSet;
 	
 	public DINLetter() throws IOException
 	{
 		document = new PDDocument();
 	}
 
-	public void generateLetter(Address from, Address to, String subject) throws IOException
+	public void setSender(Address from)
+	{
+		Calendar calendar = new GregorianCalendar();
+		this.sender = from;
+		cityAndDate = from.getCity() + ", " + calendar.get(Calendar.DAY_OF_MONTH) + "." + calendar.get(Calendar.MONTH) + "." + calendar.get(Calendar.YEAR);
+	}
+
+	public void setRecipient(Address to)
+	{
+		this.recipient = to;
+	}
+
+	public void setSubject(String subject)
+	{
+		this.subject = subject;
+	}
+
+	public void setContent(String text)
+	{
+		content = text.split("\n");
+		contentHeight = (int) (content.length*fontsize*LINESPACE);
+	}
+
+	public void setNotice(String text)
+	{
+		isNoticeSet = true;
+		notice = text.split("\n");
+	}
+
+	public void setGreeting(String greeting, Address greeter)
+	{
+		this.greeting = new String[2];
+		this.greeting[0] = greeting + ",";
+		this.greeting[1] = greeter.getFirstname() + " " + greeter.getLastname();
+	}
+
+	public void setFoldingmark(boolean active)
+	{
+		foldingmark = active;
+	}
+
+	public void save(String path) throws IOException, COSVisitorException
 	{
 		PDPage p = new PDPage(PDPage.PAGE_SIZE_A4);
 		document.addPage(p);
 
-		PDPageContentStream contentStream = new PDPageContentStream(document, p);
+		PDPageContentStream cStream = new PDPageContentStream(document, p);
 
 		// draw folding lines
-		drawFoldingLines(contentStream);
+		if(foldingmark)
+		{
+			drawFoldingLines(cStream);
+		}
 
 		// write addresses
-		writeFromAddress(contentStream,DIN_SENDER_ADDR_TOP,DIN_CONTENT_LEFT,from);
-		writeToAddress(contentStream,DIN_RCPT_ADDR_TOP,DIN_CONTENT_LEFT,to);
+		writePDFText(cStream,DIN_SENDER_ADDR_TOP,DIN_CONTENT_LEFT,fontNormal,tinyFontsize,sender.toString());
+		// TODO: underline sender address or make option for that
+		writePDFText(cStream,DIN_RCPT_ADDR_TOP,DIN_CONTENT_LEFT,fontNormal,fontsize,recipient.toLines());
 
-		// write date and location
-		writeDateCity(contentStream,DIN_CONTENT_TOP,420,from.getCity());
+		// write location and date
+		writePDFText(cStream, DIN_CONTENT_TOP,LETTER_DATE_LEFT,fontNormal,fontsize,cityAndDate);
 
-		// print subject
-		writeSubject(contentStream,LETTER_SUBJECT_TOP,DIN_CONTENT_LEFT,subject);
+		// write subject
+		writePDFText(cStream, LETTER_SUBJECT_TOP,DIN_CONTENT_LEFT,fontBold,fontsize,subject);
 
-		writeText(contentStream,LETTER_CONTENT_TOP,DIN_CONTENT_LEFT);
+		// write letter content
+		writePDFText(cStream,LETTER_CONTENT_TOP,DIN_CONTENT_LEFT,fontNormal,fontsize,content);
 
-		// print greeting
+		// write greeting
 		greetingTop -= contentHeight+25;
-		writeGreeting(contentStream,greetingTop,DIN_CONTENT_LEFT,from);
+		writePDFText(cStream,greetingTop,DIN_CONTENT_LEFT,fontNormal,fontsize,greeting);
 
 		// print notice
-		noticeTop = greetingTop-35;
-		writeNotice(contentStream,noticeTop,DIN_CONTENT_LEFT);
-
-		contentStream.close();
-	}
-
-	public void writeDateCity(PDPageContentStream pcs, int x, int y, String city) throws IOException
-	{
-		Calendar calendar = new GregorianCalendar();
-		pcs.beginText();
-		pcs.setFont(font,fontsize);
-		pcs.moveTextPositionByAmount(y,x);
-		pcs.drawString(city + ", " + calendar.get(Calendar.DAY_OF_MONTH) + "." + calendar.get(Calendar.MONTH) + "." + calendar.get(Calendar.YEAR));
-		pcs.endText();
-	}
-
-	public void writeSubject(PDPageContentStream pcs, int x, int y, String subj) throws IOException
-	{
-		pcs.beginText();
-		pcs.setFont(fontBold,fontsize);
-		pcs.moveTextPositionByAmount(y,x);
-		// TODO: subject name should not be hard coded
-		pcs.drawString("Betrifft: " + subj);
-		// TODO: line beneath subject should not be of hardcoded length
-		//pcs.drawLine(y,x-5,y+240,x-5);
-		pcs.endText();
-	}
-
-	public void writeFromAddress(PDPageContentStream pcs, int x, int y, Address addr) throws IOException
-	{
-		pcs.beginText();
-		pcs.setFont(font,tinyFontsize);
-		pcs.moveTextPositionByAmount(y,x);
-		pcs.drawString(addr.toString());
-		// TODO: make line under address generic
-		pcs.drawLine(y,x-2,y+240,x-2);
-		pcs.endText();
-	}
-
-	public void writeToAddress(PDPageContentStream pcs, int x, int y, Address addr) throws IOException
-	{
-		pcs.beginText();
-		pcs.setFont(font,fontsize);
-		pcs.moveTextPositionByAmount(y,x);
-		for(String line:addr.toLines())
+		if(isNoticeSet)
 		{
-			pcs.drawString(line);
-			pcs.moveTextPositionByAmount(0,fontsize*(-1)*linespace);
+			noticeTop = greetingTop-35;
+			writePDFText(cStream,noticeTop,DIN_CONTENT_LEFT,fontItalic,smallFontsize,notice);
 		}
-		pcs.endText();
-	}
 
-	private void drawFoldingLines(PDPageContentStream pcs) throws IOException
-	{
-		pcs.drawLine(0,DIN_FOLD1_TOP,DIN_FOLD_LENGTH,DIN_FOLD1_TOP);
-		pcs.drawLine(0,DIN_FOLD2_TOP,DIN_FOLD_LENGTH,DIN_FOLD2_TOP);
-	}
+		cStream.close();
 
-	private void writeText(PDPageContentStream pcs, int x, int y) throws IOException
-	{
-		String contentText = "Gemäß DSG 2000 begehre ich Auskunft über alle über mich gespeicherten\npersonenbezogenen Daten.\n\nDie Auskunft hat die verarbeiteten Daten, die Informationen über ihre Herkunft, allfällige\nEmpfänger oder Empfängerkreise von Übermittlungen, den Zweck der Datenverwendung\nsowie die Rechtsgrundlagen hiefür in allgemein verständlicher Form anzuführen.\n\nGemäß DSG besteht eine Verpflichtung zur Auskunft binnen 8 Wochen.";
-
-		String content[] = contentText.split("\n");
-
-		contentHeight = (int) (content.length*fontsize*linespace);
-		pcs.beginText();
-		pcs.setFont(font,fontsize);
-		pcs.moveTextPositionByAmount(y,x);
-		for(String line:content)
-		{
-			pcs.drawString(line);
-			pcs.moveTextPositionByAmount(0,fontsize*(-1)*linespace);
-		}
-		pcs.endText();
-	}
-
-	private void writeGreeting(PDPageContentStream pcs, int x, int y, Address from) throws IOException
-	{
-		pcs.beginText();
-		pcs.setFont(font,fontsize);
-		pcs.moveTextPositionByAmount(y,x);
-		// TODO: make greeting generic and not hard coded
-		pcs.drawString("Mit freundlichen Grüßen,");
-		pcs.moveTextPositionByAmount(0,fontsize*(-1)*linespace);
-		pcs.drawString(from.getFirstname() + " " + from.getLastname());
-		pcs.endText();
-	}
-
-	private void writeNotice(PDPageContentStream pcs, int x, int y) throws IOException
-	{
-		// TODO: make this notice language independant
-		String noticeText = "Hinweis: Dieses Dokument ist (mittels Bürgerkarte) mit einer qualifizierten elektronischen Signatur\nversehen, diese ist laut SigG § 4 rechtlich einer händischen Unterschrift gleichgestellt. Diese Signatur\nkann unter anderem unter http://www.signaturpruefung.gv.at überprüft werden. Eine qualifizierte\nelektronische Signatur ist, da ich als Signator unter alleiniger Kontrolle des Schlüssels bin, ein\nIdentitätsnachweis im Sinne von DSG §26 (1)";
-
-		String notice[] = noticeText.split("\n");
-
-		pcs.beginText();
-		pcs.setFont(fontItalic,smallFontsize);
-		pcs.moveTextPositionByAmount(y,x);
-		for(String line:notice)
-		{
-			pcs.drawString(line);
-			pcs.moveTextPositionByAmount(0,smallFontsize*(-1)*linespace);
-		}
-		pcs.endText();
-	}
-
-	public void saveTo(String path) throws IOException, COSVisitorException
-	{
 		// Save the newly created document
 		document.save(path);
 
@@ -190,4 +140,29 @@ public class DINLetter
 		document.close();
 	}
 
+	private void writePDFText(PDPageContentStream pcs, int x, int y, PDFont f, int fSize, String[] text) throws IOException
+	{
+		pcs.beginText();
+		pcs.setFont(f,fSize);
+		pcs.moveTextPositionByAmount(y,x);
+		for(String line:text)
+		{
+			pcs.drawString(line);
+			pcs.moveTextPositionByAmount(0,fSize*(-1)*LINESPACE);
+		}
+		pcs.endText();
+	}
+
+	private void writePDFText(PDPageContentStream pcs, int x, int y, PDFont font, int fontSize, String text) throws IOException
+	{
+		String[] textArray = new String[1];
+		textArray[0] = text;
+		writePDFText(pcs,x,y,font,fontSize,textArray);
+	}
+
+	private void drawFoldingLines(PDPageContentStream pcs) throws IOException
+	{
+		pcs.drawLine(0,DIN_FOLD1_TOP,DIN_FOLD_LENGTH,DIN_FOLD1_TOP);
+		pcs.drawLine(0,DIN_FOLD2_TOP,DIN_FOLD_LENGTH,DIN_FOLD2_TOP);
+	}
 }
