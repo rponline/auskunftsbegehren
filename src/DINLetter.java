001ae2378c7b66dcd6ad4dbd1b781a55ac344d4d
Matthias Fassl <mf@n0g.at>
2013-09-24 18:53:35 +0200

    Off-by-one Error in Month

--- src/DINLetter.java
+++ src/DINLetter.java
@@ -55,7 +55,7 @@ public class DINLetter
 	{
 		Calendar calendar = new GregorianCalendar();
 		this.sender = from;
-		cityAndDate = from.getCity() + ", " + calendar.get(Calendar.DAY_OF_MONTH) + "." + calendar.get(Calendar.MONTH) + "." + calendar.get(Calendar.YEAR);
+		cityAndDate = from.getCity() + ", " + calendar.get(Calendar.DAY_OF_MONTH) + "." + calendar.get(Calendar.MONTH)+1 + "." + calendar.get(Calendar.YEAR);
 	}
 
 	public void setRecipient(Address to)
